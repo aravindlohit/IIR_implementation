e83eaefe5c92247eec5cba10f514320dff26a95d
Aravind lohit B <aravindeee105@gmail.com>
2025-08-09 21:27:19 -0400

    Update IIR.sv

--- IIR.sv
+++ IIR.sv
@@ -1,13 +1,13 @@
-module IIR( input clk,reset, input  bit [4:0] a, output bit [4:0] y);
+module IIR( input clk,reset, input  bit [4:0] a,x, output bit [4:0] y);
 
   reg [3:0] y_val;
   wire [7:0] braugh_multiplier;
-  braugh_mult(.a(a), .b(y_val), .p(braugh_multiplier));
+  braugh_mult bm1(.a(a), .b(y_val), .p(braugh_multiplier));
 
   always @(posedge clk)
     begin
   if(reset)
-    y<=0;
+    y<=x;
   else
     y_val <= a + braugh_prod[3:0];
   
